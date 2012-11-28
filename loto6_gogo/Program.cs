92565b2c90669938b97bea5f530907e90c42ab68
neo_nezusan <neo_nezusan@mail.goo.ne.jp>
2012-11-29 00:31:56 +0900

    first attack!

---
+++ loto6_gogo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace loto6_gogo
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ loto6_gogo/main.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace loto6_gogo
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLot_01 = new System.Windows.Forms.Label();
+            this.lblLot_02 = new System.Windows.Forms.Label();
+            this.lblLot_03 = new System.Windows.Forms.Label();
+            this.lblLot_04 = new System.Windows.Forms.Label();
+            this.lblLot_05 = new System.Windows.Forms.Label();
+            this.lblLot_06 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBonus_No = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLot_01
+            // 
+            this.lblLot_01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLot_01.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblLot_01.Location = new System.Drawing.Point(22, 24);
+            this.lblLot_01.Name = "lblLot_01";
+            this.lblLot_01.Size = new System.Drawing.Size(74, 50);
+            this.lblLot_01.TabIndex = 0;
+            this.lblLot_01.Text = "99";
+            this.lblLot_01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLot_02
+            // 
+            this.lblLot_02.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLot_02.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblLot_02.Location = new System.Drawing.Point(102, 24);
+            this.lblLot_02.Name = "lblLot_02";
+            this.lblLot_02.Size = new System.Drawing.Size(74, 50);
+            this.lblLot_02.TabIndex = 1;
+            this.lblLot_02.Text = "99";
+            this.lblLot_02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLot_03
+            // 
+            this.lblLot_03.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLot_03.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblLot_03.Location = new System.Drawing.Point(182, 24);
+            this.lblLot_03.Name = "lblLot_03";
+            this.lblLot_03.Size = new System.Drawing.Size(74, 50);
+            this.lblLot_03.TabIndex = 2;
+            this.lblLot_03.Text = "99";
+            this.lblLot_03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLot_04
+            // 
+            this.lblLot_04.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLot_04.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblLot_04.Location = new System.Drawing.Point(262, 24);
+            this.lblLot_04.Name = "lblLot_04";
+            this.lblLot_04.Size = new System.Drawing.Size(74, 50);
+            this.lblLot_04.TabIndex = 3;
+            this.lblLot_04.Text = "99";
+            this.lblLot_04.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLot_05
+            // 
+            this.lblLot_05.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLot_05.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblLot_05.Location = new System.Drawing.Point(342, 24);
+            this.lblLot_05.Name = "lblLot_05";
+            this.lblLot_05.Size = new System.Drawing.Size(74, 50);
+            this.lblLot_05.TabIndex = 4;
+            this.lblLot_05.Text = "99";
+            this.lblLot_05.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLot_06
+            // 
+            this.lblLot_06.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLot_06.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblLot_06.Location = new System.Drawing.Point(422, 24);
+            this.lblLot_06.Name = "lblLot_06";
+            this.lblLot_06.Size = new System.Drawing.Size(74, 50);
+            this.lblLot_06.TabIndex = 5;
+            this.lblLot_06.Text = "99";
+            this.lblLot_06.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(347, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Bonus No:";
+            // 
+            // lblBonus_No
+            // 
+            this.lblBonus_No.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblBonus_No.Font = new System.Drawing.Font("MS UI Gothic", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.lblBonus_No.Location = new System.Drawing.Point(422, 84);
+            this.lblBonus_No.Name = "lblBonus_No";
+            this.lblBonus_No.Size = new System.Drawing.Size(74, 50);
+            this.lblBonus_No.TabIndex = 7;
+            this.lblBonus_No.Text = "99";
+            this.lblBonus_No.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(462, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "ちぇっく！";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(521, 211);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblBonus_No);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLot_06);
+            this.Controls.Add(this.lblLot_05);
+            this.Controls.Add(this.lblLot_04);
+            this.Controls.Add(this.lblLot_03);
+            this.Controls.Add(this.lblLot_02);
+            this.Controls.Add(this.lblLot_01);
+            this.Name = "main";
+            this.Text = "loto6 gogo!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLot_01;
+        private System.Windows.Forms.Label lblLot_02;
+        private System.Windows.Forms.Label lblLot_03;
+        private System.Windows.Forms.Label lblLot_04;
+        private System.Windows.Forms.Label lblLot_05;
+        private System.Windows.Forms.Label lblLot_06;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBonus_No;
+        private System.Windows.Forms.Button button1;
+    }
+}
